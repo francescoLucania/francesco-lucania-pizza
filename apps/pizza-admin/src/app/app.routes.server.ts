399d87f66a2a7722f8257d03e173823ab1e0eeb8
francescoLucania <35537086+francescoLucania@users.noreply.github.com>
2026-01-16 11:35:28 +0300

    Исправлены ошибки билдера и типизации

--- apps/pizza-admin/src/app/app.routes.server.ts
+++ apps/pizza-admin/src/app/app.routes.server.ts
@@ -1,8 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import {Home} from "./views/home/home";
+import {Registration} from "./views/registration/registration";
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'registration',
     renderMode: RenderMode.Prerender,
   },
 ];
--- apps/pizza-admin/src/app/app.routes.ts
+++ apps/pizza-admin/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Route } from '@angular/router';
 import {Home} from "./views/home/home";
+import {Registration} from "./views/registration/registration";
+import {RenderMode} from "@angular/ssr";
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'registration',
+    component: Registration,
   },
 ];
